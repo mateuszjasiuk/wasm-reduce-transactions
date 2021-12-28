00abe9bff37b6e6178b42ae0f54997fd83ca7d9c
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2021-12-28 21:03:54 +0100

    feat: algorithm

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import {Graph} from "wasm-reduce-transactions";
 
+function chunks (buffer, chunkSize) {
+	var result = [];
+	var len = buffer.byteLength;
+	var i = 0;
 
-const g = Graph.new(3);
+	while (i < len) {
+		result.push(buffer.slice(i, i += chunkSize));
+	}
+
+	return result;
+}
+
+const g = new Graph(3);
 
 g.add_edge(0, 1, 3);
-g.add_edge(0, 2, 500);
+g.add_edge(0, 2, 5);
+
 
 let a = g.reduce();
 
-console.log(a);
+let dataview = new DataView(a.buffer);
+let int32be = dataview.getInt32(0);
 
-// g.render();
+console.log(chunks(a.buffer, 4));
+
+chunks(a.buffer, 4).forEach(buffer => {
+  var dv = new DataView(buffer, 0);
+  console.log("===")
+  console.log("user:", dv.getInt8(3))
+  var dv2 = new DataView(buffer, 1, 2);
+  console.log("pays", dv2.getInt16(0, true))
+  console.log("user:",dv.getInt8(0))
+})
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-// mod utils;
+mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::fmt;
-use std::iter::once;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -15,8 +14,27 @@ pub struct Graph {
     data: Vec<u8>,
 }
 
+#[wasm_bindgen]
+pub struct TupleTransaction(pub u8, pub u8, pub i16);
+
 impl Graph {
-    pub fn net_amount(&self) -> Vec<i16> {
+    fn min_cash_flow<'a>(&self, net: &'a mut Vec<i16>, transactions: &'a mut Vec<(u8, i16, u8)>) -> &'a mut Vec<(u8, i16, u8)> {
+        let (min_index, min_value) = self.get_min(&net);
+        let (max_index, max_value) = self.get_max(&net);
+
+        if net[min_index as usize] == 0 && net[max_index as usize] == 0 {
+            return transactions;
+        }
+
+        let min_of_2 = if -1 * min_value > max_value { max_value } else { -1 * min_value };
+        net[min_index as usize] += min_of_2;
+        net[max_index as usize] -= min_of_2;
+        transactions.push((min_index, min_of_2, max_index));
+
+        return self.min_cash_flow(net, transactions);
+    }
+
+    fn net_amount(&self) -> Vec<i16> {
         let mut net: Vec<i16> = vec![];
         let mut counter: usize = self.data[0] as usize;
 
@@ -39,40 +57,22 @@ impl Graph {
         return net;
     }
 
-    fn get_min(&self, net: &Vec<i16>) -> (usize, i16) {
+    fn get_min(&self, net: &Vec<i16>) -> (u8, i16) {
         let a = *net.iter().min().unwrap();
         let i = net.iter().position(|&r| r == a).unwrap();
-        return (i, a);
+        return (i as u8, a);
     }
 
-    fn get_max(&self, net: &Vec<i16>) -> (usize, i16) {
+    fn get_max(&self, net: &Vec<i16>) -> (u8, i16) {
         let a = *net.iter().max().unwrap();
         let i = net.iter().position(|&r| r == a).unwrap();
-        return (i, a);
-    }
-
-    pub fn min_cash_flow<'a>(&self, net: &'a mut Vec<i16>, transactions: &'a mut Vec<(usize, i16, usize)>) -> &'a mut Vec<(usize, i16, usize)> {
-        let min = &self.get_min(&net);
-        let max = &self.get_max(&net);
-
-        if net[min.0] == 0 && net[max.0] == 0 {
-            //TODO: return new graph
-            return transactions;
-        }
-
-        let min_of_2 = if -1 * min.1 > max.1  {-1 * min.1 } else {max.1};
-        net[min.0] += min_of_2;
-        net[max.0] -= min_of_2;
-        transactions.push((min.0, min_of_2, max.0));
-        println!("Person {} pays {} to Person {}", min.0, min_of_2, max.0);
-
-        return self.min_cash_flow(net, transactions);
+        return (i as u8, a);
     }
 }
 
 #[wasm_bindgen]
 impl Graph {
-
+    #[wasm_bindgen(constructor)]
     pub fn new(number_of_nodes: u8) -> Graph {
         let mut data: Vec<u8> = vec![];
         let mem_size = number_of_nodes;
@@ -86,7 +86,7 @@ impl Graph {
         Graph { data }
     }
 
-    pub fn add_edge(&mut self, u: u8, v: u8, wt: u16) {
+    pub fn add_edge(&mut self, u: u8, v: u8, wt: i16) {
         self.data[(u + 1) as usize] += 1;
         let [wt_1, wt_2] = wt.to_be_bytes();
         self.data.extend_from_slice(&[v, wt_1, wt_2]);
@@ -96,15 +96,18 @@ impl Graph {
         self.to_string()
     }
 
-    pub fn reduce(&self) -> Vec<i16> {
+    pub fn reduce(&self) -> Vec<u32> {
         let mut net = self.net_amount();
         let mut transactions = vec![];
 
-        let tran = self.min_cash_flow(&mut net, &mut transactions).clone();
+        let cash_flow = self.min_cash_flow(&mut net, &mut transactions).clone();
 
-        return tran.iter()
-            .flat_map(|tup| once(tup.0 as i16).chain(once(tup.1)).chain(once(tup.2 as i16)))
-            .collect();
+        let mut binary_transactions: Vec<u32> = vec![];
+        for x in cash_flow.iter() {
+            binary_transactions.push(((x.0 as u32) << 24) | (x.1 as u32) << 8 | x.2 as u32);
+        }
+
+        return binary_transactions;
     }
 }
 
