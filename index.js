219a442345ff04010aa929b2afbf2163d4ec5061
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2022-01-13 15:40:47 +0100

    feat: change to cents

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ function chunks (buffer, chunkSize) {
 
 const g = new TransactionsGraph(3);
 
-g.add_edge(0, 1, 3);
-g.add_edge(0, 2, 5);
-
+g.add_edge(0, 1, 320);
+g.add_edge(0, 2, 550);
 
 let a = g.reduce();
 
@@ -25,11 +24,11 @@ let int32be = dataview.getInt32(0);
 
 console.log(chunks(a.buffer, 4));
 
-chunks(a.buffer, 4).forEach(buffer => {
+chunks(a.buffer, 6).forEach(buffer => {
   var dv = new DataView(buffer, 0);
   console.log("===")
-  console.log("user:", dv.getInt8(3))
-  var dv2 = new DataView(buffer, 1, 2);
+  console.log("user:", dv.getInt8(5))
+  var dv2 = new DataView(buffer, 1, 4);
   console.log("pays", dv2.getInt16(0, true))
   console.log("user:",dv.getInt8(0))
 })
--- src/lib.rs
+++ src/lib.rs
@@ -9,66 +9,50 @@ use std::fmt;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-type Transactions = Vec<u8>;
-type From = u8;
-type Amount = i16;
-type To = u8;
-type CashFlow = (From, Amount, To);
+type Index = u8;
+type From = Index;
+type To = Index;
+type Cents = i32;
+type Net = Vec<Cents>;
+type CashFlow = (From, Cents, To);
 
+const ZERO: i32 = 0;
 
 #[wasm_bindgen]
 pub struct TransactionsGraph {
-    transactions: Transactions,
+    net: Net,
 }
 
 impl TransactionsGraph {
-    fn min_cash_flow<'a>(&self, net: &'a mut Vec<i16>, transactions: &'a mut Vec<CashFlow>) -> &'a mut Vec<CashFlow> {
-        let (min_index, min_value) = self.get_min(&net);
-        let (max_index, max_value) = self.get_max(&net);
+    fn min_cash_flow<'a>(&mut self, transactions: &'a mut Vec<CashFlow>) -> &'a mut Vec<CashFlow> {
+        let (min_index, min_value) = self.get_min(&self.net);
+        let (max_index, max_value) = self.get_max(&self.net);
 
-        if net[usize::from(min_index)] == 0 && net[max_index as usize] == 0 {
+        if self.net[usize::from(min_index)] == ZERO && self.net[max_index as usize] == ZERO {
             return transactions;
         }
 
-        let min_of_2 = if -1 * min_value > max_value { max_value } else { -1 * min_value };
-        net[usize::from(min_index)] += min_of_2;
-        net[usize::from(max_index)] -= min_of_2;
+        let min_of_2 = if min_value * -1 > max_value {
+            max_value
+        } else { 
+            min_value * -1
+        };
+
+        self.net[usize::from(min_index)] += min_of_2;
+        self.net[usize::from(max_index)] -= min_of_2;
         transactions.push((min_index, min_of_2, max_index));
 
-        return self.min_cash_flow(net, transactions);
+        return self.min_cash_flow(transactions);
     }
 
-    fn net_amount(&self) -> Vec<i16> {
-        let mut net: Vec<i16> = vec![];
-        let num_of_transactions = usize::from(self.transactions[0]);
-        let mut counter = usize::from(self.transactions[0]);
-
-        for _ in 0..num_of_transactions {
-            net.push(0);
-        }
-
-        for transaction_from in 0..num_of_transactions {
-            for transaction_to_index in 0..self.transactions[transaction_from + 1] {
-                let transaction_to = counter + usize::from(transaction_to_index) * 3 + 1;
-
-                let number = (i16::from(self.transactions[transaction_to + 1]) << 8) | i16::from(self.transactions[transaction_to + 2]);
-                net[transaction_from] -= number;
-                net[self.transactions[transaction_to] as usize] += number;
-            }
-            counter = counter + usize::from(self.transactions[transaction_from + 1]) * 3;
-        }
-
-        return net;
-    }
-
-    fn get_min(&self, net: &Vec<i16>) -> (u8, i16) {
+    fn get_min(&self, net: &Net) -> (Index, Cents) {
         let a = *net.iter().min().unwrap();
         let i: usize = net.iter().position(|&r| r == a).unwrap();
         // Unsafe kind of
         return (i as u8, a);
     }
 
-    fn get_max(&self, net: &Vec<i16>) -> (u8, i16) {
+    fn get_max(&self, net: &Net) -> (Index, Cents) {
         let a = *net.iter().max().unwrap();
         let i = net.iter().position(|&r| r == a).unwrap();
         // Unsafe kind of
@@ -79,38 +63,37 @@ impl TransactionsGraph {
 #[wasm_bindgen]
 impl TransactionsGraph {
     #[wasm_bindgen(constructor)]
-    pub fn new(number_of_nodes: u8) -> TransactionsGraph {
-        let mut transactions: Transactions = vec![];
-        let mem_size = number_of_nodes;
+    pub fn new(number_of_nodes: Index) -> TransactionsGraph {
+        let mut net: Net = vec![];
 
-        transactions.push(number_of_nodes);
-
-        for _ in 0..mem_size {
-            transactions.push(0);
+        for _ in 0..number_of_nodes {
+            net.push(ZERO);
         }
 
-        TransactionsGraph { transactions }
+        TransactionsGraph { net }
     }
 
-    pub fn add_edge(&mut self, u: u8, v: u8, wt: i16) {
-        self.transactions[(u + 1) as usize] += 1;
-        let [wt_1, wt_2] = wt.to_be_bytes();
-        self.transactions.extend_from_slice(&[v, wt_1, wt_2]);
+    pub fn add_edge(&mut self, u: Index, v: Index, cents: Cents) {
+        self.net[usize::from(u)] += cents;
+        self.net[usize::from(v)] -= cents;
     }
 
     pub fn render(&self) -> String {
         self.to_string()
     }
 
-    pub fn reduce(&self) -> Vec<u32> {
-        let mut net = self.net_amount();
+    pub fn reduce(&mut self) -> Vec<u8> {
         let mut transactions = vec![];
+        let cash_flow = self.min_cash_flow(&mut transactions);
+        let mut binary_transactions: Vec<u8> = vec![];
 
-        let cash_flow = self.min_cash_flow(&mut net, &mut transactions).clone();
-
-        let mut binary_transactions: Vec<u32> = vec![];
         for x in cash_flow.iter() {
-            binary_transactions.push(((x.0 as u32) << 24) | (x.1 as u32) << 8 | x.2 as u32);
+            binary_transactions.push(x.0);
+            binary_transactions.push(x.1 as u8);
+            binary_transactions.push((x.1 >> 8) as u8);
+            binary_transactions.push((x.1 >> 16) as u8);
+            binary_transactions.push((x.1 >> 24) as u8);
+            binary_transactions.push(x.2);
         }
 
         return binary_transactions;
@@ -120,10 +103,13 @@ impl TransactionsGraph {
 
 impl fmt::Display for TransactionsGraph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for byte in self.transactions.iter() {
-            write!(f, "[{}]", byte)?;
+        for (i, wt) in self.net.iter().enumerate() {
+            write!(f, "User: {} {}", i, wt.to_string())?;
         }
 
         Ok(())
     }
 }
+
+//TODO:
+//write tests
